cd6121c049b7c3724f7cc1da4ac470f5e7af5644
Suneom <dkxl0701@gmail.com>
2022-01-11 12:38:17 +0900

    Fixed Navigation bar error

--- MyMusicRecord/ViewController/ViewController.swift
+++ MyMusicRecord/ViewController/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        if let _ = self.navigationController?.navigationBar.items?[0].title {
+            self.navigationController?.navigationBar.items?[0].title = "My Music Record"
+        }
+    }
 
 }
 
