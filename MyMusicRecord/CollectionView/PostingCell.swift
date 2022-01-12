09c327030774fbb737c507ae07850c04747d1d8c
Suneom <dkxl0701@gmail.com>
2022-01-12 20:52:19 +0900

    Updated collection view

---
+++ MyMusicRecord/CollectionView/PostingCell.swift
@@ -0,0 +1,12 @@
+//
+//  PostingCell.swift
+//  MyMusicRecord
+//
+//  Created by 엄태양 on 2022/01/12.
+//
+
+import UIKit
+
+class PostingCell: UICollectionViewCell {
+    
+}
---
+++ MyMusicRecord/CollectionView/RecentHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  RecentHeaderView.swift
+//  MyMusicRecord
+//
+//  Created by 엄태양 on 2022/01/12.
+//
+
+import UIKit
+
+class RecentHeaderView: UICollectionReusableView {
+        
+}
