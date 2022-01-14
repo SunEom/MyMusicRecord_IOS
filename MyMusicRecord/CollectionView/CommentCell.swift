5653fff2a7f944ecc02a2e6ee8e72c34681e9e6b
Suneom <dkxl0701@gmail.com>
2022-01-14 16:25:00 +0900

    Created Comment Type

---
+++ MyMusicRecord/CollectionView/CommentCell.swift
@@ -0,0 +1,12 @@
+//
+//  CommentCell.swift
+//  MyMusicRecord
+//
+//  Created by 엄태양 on 2022/01/14.
+//
+
+import UIKit
+
+class CommentCell: UICollectionViewCell {
+    
+}
---
+++ MyMusicRecord/Type/Comment.swift
@@ -0,0 +1,18 @@
+//
+//  Comment.swift
+//  MyMusicRecord
+//
+//  Created by 엄태양 on 2022/01/14.
+//
+
+import Foundation
+
+struct Comment {
+    var postNum: Int
+    var commentNum: Int
+    var contents: String
+    var writtenDate: Date
+    var updatedDate: Date
+    var commentID: Int
+    var nickname: String
+}
