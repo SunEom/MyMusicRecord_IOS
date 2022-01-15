7163d3b1414e09ebec3fb487587686da50147c4f
Suneom <dkxl0701@gmail.com>
2022-01-15 21:10:14 +0900

    Created Login UI

---
+++ MyMusicRecord/ViewController/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  MyMusicRecord
+//
+//  Created by 엄태양 on 2022/01/15.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
